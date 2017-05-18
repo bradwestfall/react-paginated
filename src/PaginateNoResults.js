1c5f8712f6fe19828fdcd6083ddcf45ab981ec2c
Brad Westfall <brad@azpixels.com>
2017-05-18 08:58:16 -0700

    wrap PaginateNoResults in a div

--- src/PaginateNoResults.js
+++ src/PaginateNoResults.js
@@ -1,3 +1,13 @@
-const PaginateNoResults = props => props.children
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+const PaginateNoResults = props => (
+  <div className={classnames('react-paginated-no-results', props.className)}>{props.children}</div>
+)
+
+PaginateNoResults.propTypes = {
+  className: PropTypes.string
+}
 
 export default PaginateNoResults
