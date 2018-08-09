2448c174afc57e78bb28228b39a42848a4bc1979
Brad Westfall <brad@azpixels.com>
2018-08-09 14:30:12 -0700

    a new way of doing PaginateNav, breaks old way

--- src/PaginateNav.js
+++ src/PaginateNav.js
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-const PaginateNav = ({ children, totalResults, results, page, resultsPerPage, fragment, ...rest }) => {
+const PaginateNav = ({ children, totalResults, page, resultsPerPage, range, fragment, ...rest }) => {
   const totalPages = Math.ceil(totalResults / resultsPerPage)
-  const links = []
-  if (totalPages === 1) return null
-  for (var i = 1; i <= totalPages; i++) {
-    const active = i === page
-    links.push(children(i, active))
-  }
-  return fragment ? <Fragment>{links}</Fragment> : <div {...rest}>{links}</div>
+  const totalRange = (range * 2) + 1
+
+  let rangeStart = page - range >= 1 ? page - range : 1
+  let rangeEnd = page + range <= totalPages ? page + range : totalPages
+  rangeStart = rangeEnd - rangeStart < totalPages && rangeEnd === totalPages ? totalPages - totalRange : rangeStart
+  rangeEnd = rangeEnd - rangeStart < totalRange && rangeStart === 1 ? totalRange : rangeEnd
+
+  const args = { totalResults, page, resultsPerPage, totalPages, range, totalRange, rangeStart, rangeEnd }
+
+  return fragment ? <Fragment>{children(args)}</Fragment> : <div {...rest}>{children(args)}</div>
 }
 
 PaginateNav.defaultProps = {
